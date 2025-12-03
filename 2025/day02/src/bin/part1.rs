d1ae4a91cd13ad62bf26405aa1a550699ff0c925
Tim Spickermann <tim@timsp.de>
2025-12-03 10:08:12 +0100

    day2

---
+++ 2025/day02/src/bin/part1.rs
@@ -0,0 +1,74 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = run(input);
+    print!("{}\n", output);
+}
+
+fn run(input: &str) -> String {
+    let mut counter = 0;
+    for range in input.split(",") {
+        let mut split = range.split("-");
+        let from = split.next().unwrap();
+        let to = split.next().unwrap();
+        for len in from.len()..=to.len() {
+            if len % 2 != 0 {
+                continue;
+            } // skip as can never be invalid
+            let from = if from.len() == len {
+                from.to_string()
+            } else {
+                format!("1{}", "0".repeat(len - 1))
+            };
+            let to = if to.len() == len {
+                to.to_string()
+            } else {
+                "9".repeat(len)
+            };
+            check_range(&mut counter, &from, &to);
+        }
+    }
+    counter.to_string()
+}
+
+fn check_range(counter: &mut usize, from: &str, to: &str) {
+    // println!("checking range from {from} to {to}");
+    for num in from.parse::<usize>().unwrap()..=to.parse::<usize>().unwrap() {
+        if !is_valid(num.to_string()) {
+            *counter += num;
+        }
+    }
+}
+
+fn is_valid(num: String) -> bool {
+    let mid = num.len() / 2;
+    let (a, b) = num.split_at(mid);
+    a != b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_valid_works() {
+        assert_eq!(is_valid("11".to_string()), false);
+        assert_eq!(is_valid("9999".to_string()), false);
+        assert_eq!(is_valid("38593859".to_string()), false);
+        assert_eq!(is_valid("12345".to_string()), true);
+        assert_eq!(is_valid("1698522".to_string()), true);
+    }
+
+    #[test]
+    fn parts_work() {
+        assert_eq!(run("11-22"), "33");
+        assert_eq!(run("95-115"), "99");
+        assert_eq!(run("998-1012"), "1010");
+        assert_eq!(run("1188511880-1188511890"), "1188511885");
+    }
+
+    #[test]
+    fn it_works() {
+        let result = run("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        assert_eq!(result, "1227775554".to_string());
+    }
+}
